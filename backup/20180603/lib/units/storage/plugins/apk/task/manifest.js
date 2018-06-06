54bfc721e366bc5e85386952ed049abfc28755e0
ss ss <3905902@qq.com>
2018-06-06 18:40:57 +0800

    remove files

--- backup/20180603/lib/units/storage/plugins/apk/task/manifest.js
+++
@@ -1,7 +0,0 @@
-var ApkReader = require('adbkit-apkreader')
-
-module.exports = function(file) {
-  return ApkReader.open(file.path).then(function(reader) {
-    return reader.readManifest()
-  })
-}
--- backup/20180603/res/bower_components/angular-dialog-service/index.js
+++
@@ -1,3 +0,0 @@
-require('./dist/dialogs.min.js');
-require('./dist/dialogs-default-translations.min.js');
-module.exports = 'dialogs.main';
